538208e56e0846d9908b7fa52d6b69e23b2863b4
Embedthis Software <dev@embedthis.com>
2012-12-25 16:29:07 -0800

    Fix compiler warning

--- src/debug.c
+++ src/debug.c
@@ -113,8 +113,7 @@ void debug_print_mpi(ssl_context * ssl, int level,
         if (X->p[n] != 0)
             break;
 
-    snprintf(str, maxlen, "%s(%04d): value of '%s' (%lu bits) is:\n",
-         file, line, text, ((n + 1) * sizeof(t_int)) << 3);
+    snprintf(str, maxlen, "%s(%04d): value of '%s' (%u bits) is:\n", file, line, text, ((n + 1) * sizeof(t_int)) << 3);
 
     str[maxlen] = '\0';
     ssl->f_dbg(ssl->p_dbg, level, str);
