2837761d1af4a21fc08eeef991779d199bc7e375
Embedthis Software <dev@embedthis.com>
2013-01-07 21:40:03 -0800

    More PUBLIC work

--- src/ssl.h
+++ src/ssl.h
@@ -256,7 +256,7 @@ struct _ssl_context {
 extern "C" {
 #endif
 
-    PUBLIC int ssl_default_ciphers[];
+    extern int ssl_default_ciphers[];
 
     /**
      * \brief          Initialize an SSL context
