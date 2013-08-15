ebb6be9f72176930e36ebd10fc4489918d224059
Embedthis Software <dev@embedthis.com>
2013-08-14 18:14:47 -0700

    Regen projects

--- projects/est-vxworks-default-bit.h
+++ projects/est-vxworks-default-bit.h
@@ -101,10 +101,10 @@
     #define BIT_EST_XTEA 1
 #endif
 #ifndef BIT_HAS_ATOMIC
-    #define BIT_HAS_ATOMIC 1
+    #define BIT_HAS_ATOMIC 0
 #endif
 #ifndef BIT_HAS_ATOMIC64
-    #define BIT_HAS_ATOMIC64 1
+    #define BIT_HAS_ATOMIC64 0
 #endif
 #ifndef BIT_HAS_DOUBLE_BRACES
     #define BIT_HAS_DOUBLE_BRACES 0
